fada1923870a15203444ca3d0635bd323591eea7
XIEJIN-PC\Jeffrey <dandandexihuanmeizi@gmail.com>
2017-12-06 16:28:47 +0800

    第一次拉取

--- app/src/main/java/com/example/xiejin/kotlinlearning/MySrollImageView.java
+++ app/src/main/java/com/example/xiejin/kotlinlearning/MySrollImageView.java
@@ -13,7 +13,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.PagerSnapHelper;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageView;
 
 import java.lang.ref.WeakReference;
@@ -59,7 +58,6 @@ public class MySrollImageView extends AppCompatImageView {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         mMinDy = h;
-        Log.d("fuck", "change");
     }
 
     private Bitmap getDrawableToBitmap() {
